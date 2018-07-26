81cc4cc8d750e76e9abee8cd49951b61c2398c68
okynos <pylott@gmail.com>
2018-07-26 14:43:26 +0200

    Adding path to generated file

--- tests/syscheck/01-whodata/test/whodata_generator.py
+++ tests/syscheck/01-whodata/test/whodata_generator.py
@@ -3,9 +3,10 @@ import time
 import sys
 
 def generate_whodata_alert():
-  limit = int(sys.argv[1])
+  limit = int(sys.argv[2])
+  filename = sys.argv[1]
   for i in range(limit):
-    whodata_file = open("whodata.txt", "w")
+    whodata_file = open(filename, "w")
     whodata_file.write(str(i))
     print(i)
     whodata_file.close()
