af336b5eafb64b7d63d912618a2db9b3064a8990
okynos <pylott@gmail.com>
2018-07-27 12:55:44 +0200

    Fix array in manager test, add creation of test file to avoid false alert

--- tests/syscheck/01-whodata/test/whodata_test.py
+++ tests/syscheck/01-whodata/test/whodata_test.py
@@ -15,15 +15,15 @@ num_agents = int(sys.argv[4])
 total_ctr = 0
 audit_ctr = 0
 
-agents = []
+agents = [None] * (num_agents+1)
 
-for i in range(num_agents+4):
+for i in range(len(agents)):
   obj = {}
-  obj['alerts_generated'] = 0
-  obj['alerts_whodata'] = 0
+  obj['generated_alerts'] = 0
+  obj['whodata_alerts'] = 0
   obj['id'] = 0
   obj['name'] = ""
-  agents.append(obj.copy())
+  agents[i] = obj.copy()
 
 with open(json_file) as input_file:
   for line in input_file:
@@ -37,8 +37,9 @@ with open(json_file) as input_file:
           and 'effective_user' in alert['syscheck']['audit']
           and 'user' in alert['syscheck']['audit']
           and 'group' in alert['syscheck']['audit']
+          and alert['syscheck']['path'] == test_filepath 
+          and alert['syscheck']['event'] == test_eventtype
         ):
-      if alert['syscheck']['path'] == test_filepath and alert['syscheck']['event'] == test_eventtype:
         audit_ctr += 1
         agentid = int(alert['agent']['id'])
         agents[agentid]['name'] = alert['agent']['name']
@@ -49,9 +50,6 @@ with open(json_file) as input_file:
       agentid = int(alert['agent']['id'])
       agents[agentid]['alerts_generated'] += 1
 
-for agent in agents:
-  if agent['id'] == 0:
-    agents.remove(agent)
 
 f = open(result_file, 'w')
 f.write("Total alerts in file: " + str(total_ctr) + "\n")
