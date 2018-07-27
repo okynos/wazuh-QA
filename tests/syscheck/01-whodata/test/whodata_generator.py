7cbc18aa3f1fb46f280dc7970ea1c4b13d5406c7
okynos <pylott@gmail.com>
2018-07-27 16:43:13 +0200

    Improve test generator and ansible test

--- tests/syscheck/01-whodata/test/whodata_generator.py
+++ tests/syscheck/01-whodata/test/whodata_generator.py
@@ -5,12 +5,13 @@ import sys
 def generate_whodata_alert():
   limit = int(sys.argv[2])
   filename = sys.argv[1]
+  interval = int(sys.argv[3])
   for i in range(limit):
-    whodata_file = open(filename, "w")
-    whodata_file.write(str(i))
+    whodata_file = open(filename, "a")
+    whodata_file.write(str(i)+"\n")
     print(i)
     whodata_file.close()
-    time.sleep(1)
+    time.sleep(interval)
 
 
 generate_whodata_alert()
