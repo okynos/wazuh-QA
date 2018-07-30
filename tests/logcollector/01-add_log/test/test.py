18ce5cb6426fdb847bb0d6b90a10232758dd04b6
okynos <pylott@gmail.com>
2018-07-30 18:49:06 +0200

    Added logcollector command prevention test, fixing hardcoded strings in scripts and Added previous syscheck test

--- tests/logcollector/01-add_log/test/test.py
+++ tests/logcollector/01-add_log/test/test.py
@@ -1,8 +1,8 @@
 # test1AddLog.py
-def test_AddLogAgent():
+def test_agent():
         f = open('/var/ossec/logs/ossec.log', 'r')
         assert "ssh.log" in f.read()
 
-def test_AlertInManager():
+def test_manager():
         f = open('/var/ossec/logs/alerts/alerts.log', 'r')
         assert "Jun 27 14:35:27 localhost sshd[1258]: pam_unix(sshd:session): session opened for user root by (uid=0)" in f.read()
