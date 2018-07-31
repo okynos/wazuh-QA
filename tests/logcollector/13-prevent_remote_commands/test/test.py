48d3634f7aa92ffec210a8a95045dd3ae05bcbe8
okynos <pylott@gmail.com>
2018-07-31 11:30:57 +0200

    Add other directory to launch ls test

--- tests/logcollector/13-prevent_remote_commands/test/test.py
+++ tests/logcollector/13-prevent_remote_commands/test/test.py
@@ -1,13 +1,13 @@
 # test1AddLog.py
 def test_agent():
         f = open('/var/ossec/logs/ossec.log', 'r')
-        assert "Monitoring output of command(10): ls -l /13-prevent_remote_commands" in f.read()
+        assert "Monitoring output of command(10): ls -l /13-prevent_remote_commands/ls_test_dir" in f.read()
         f.close()
         f = open('/var/ossec/logs/ossec.log', 'r')
         assert "Monitoring full output of command(10): cat /13-prevent_remote_commands/test.conf" in f.read()
         f.close()
 
 def test_manager():
-        f = open('/var/ossec/logs/alerts/alerts.log', 'r')
+        f = open('/var/ossec/logs/archives/archives.json', 'r')
         assert "Jun 27 14:35:27 localhost sshd[1258]: pam_unix(sshd:session): session opened for user root by (uid=0)" in f.read()
         f.close()
