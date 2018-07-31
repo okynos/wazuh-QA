f817c00451f9c706dd85bb498571cc3dcdc35558
okynos <pylott@gmail.com>
2018-07-31 11:18:13 +0200

    Fix file close in pytest

--- tests/logcollector/01-add_log/test/test.py
+++ tests/logcollector/01-add_log/test/test.py
@@ -2,7 +2,9 @@
 def test_agent():
         f = open('/var/ossec/logs/ossec.log', 'r')
         assert "ssh.log" in f.read()
+        f.close()
 
 def test_manager():
         f = open('/var/ossec/logs/alerts/alerts.log', 'r')
         assert "Jun 27 14:35:27 localhost sshd[1258]: pam_unix(sshd:session): session opened for user root by (uid=0)" in f.read()
+        f.close()
--- tests/logcollector/13-prevent_remote_commands/test/test.py
+++ tests/logcollector/13-prevent_remote_commands/test/test.py
@@ -2,8 +2,12 @@
 def test_agent():
         f = open('/var/ossec/logs/ossec.log', 'r')
         assert "Monitoring output of command(10): ls -l /13-prevent_remote_commands" in f.read()
+        f.close()
+        f = open('/var/ossec/logs/ossec.log', 'r')
         assert "Monitoring full output of command(10): cat /13-prevent_remote_commands/test.conf" in f.read()
+        f.close()
 
 def test_manager():
         f = open('/var/ossec/logs/alerts/alerts.log', 'r')
         assert "Jun 27 14:35:27 localhost sshd[1258]: pam_unix(sshd:session): session opened for user root by (uid=0)" in f.read()
+        f.close()
