823487a52daf01e487cb92109203cf0c0cec80e3
okynos <pylott@gmail.com>
2018-07-31 12:24:22 +0200

    Changing cat test file, simplify

--- tests/logcollector/13-prevent_remote_commands/test/test.py
+++ tests/logcollector/13-prevent_remote_commands/test/test.py
@@ -4,7 +4,7 @@ def test_agent():
         assert "Monitoring output of command(10): ls -l /13-prevent_remote_commands/ls_test_dir/ --time-style=+" in f.read()
         f.close()
         f = open('/var/ossec/logs/ossec.log', 'r')
-        assert "Monitoring full output of command(10): cat /13-prevent_remote_commands/test.conf" in f.read()
+        assert "Monitoring full output of command(10): cat /13-prevent_remote_commands/cat_file.txt" in f.read()
         f.close()
 
 def test_manager():
@@ -15,5 +15,5 @@ def test_manager():
         assert "ossec: output: 'ls -l /13-prevent_remote_commands/ls_test_dir/ --time-style=+': -rw-r--r-- 1 root root 396  test.conf" in f.read()
         f.close()
         f = open('/var/ossec/logs/archives/archives.json', 'r')
-        assert "ossec: output: 'cat /13-prevent_remote_commands/test.conf':\n<ossec_config>\n\n  <localfile>\n    <log_format>command</log_format>\n    <command>ls -l /13-prevent_remote_commands/ls_test_dir/ --time-style=+</command>\n    <frequency>10</frequency>\n  </localfile>\n\n  <localfile>\n    <log_format>full_command</log_format>\n    <command>cat /13-prevent_remote_commands/test.conf</command>\n    <frequency>10</frequency>\n  </localfile>\n\n</ossec_config>" in f.read()
+        assert "ossec: output: 'cat /13-prevent_remote_commands/cat_file.txt':cat test" in f.read()
         f.close()
