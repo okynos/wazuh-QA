bc3c57957c2047e4b69f129182dcd63a9f0c2ed6
okynos <pylott@gmail.com>
2018-07-31 11:46:47 +0200

    Remove time in ls command

--- tests/logcollector/13-prevent_remote_commands/test/test.py
+++ tests/logcollector/13-prevent_remote_commands/test/test.py
@@ -1,7 +1,7 @@
 # test1AddLog.py
 def test_agent():
         f = open('/var/ossec/logs/ossec.log', 'r')
-        assert "Monitoring output of command(10): ls -l /13-prevent_remote_commands/ls_test_dir" in f.read()
+        assert "Monitoring output of command(10): ls -l /13-prevent_remote_commands/ls_test_dir --time-style=+" in f.read()
         f.close()
         f = open('/var/ossec/logs/ossec.log', 'r')
         assert "Monitoring full output of command(10): cat /13-prevent_remote_commands/test.conf" in f.read()
@@ -9,10 +9,10 @@ def test_agent():
 
 def test_manager():
         f = open('/var/ossec/logs/archives/archives.json', 'r')
-        assert "ossec: output: 'ls -l /13-prevent_remote_commands/ls_test_dir/': total 4" in f.read()
+        assert "ossec: output: 'ls -l /13-prevent_remote_commands/ls_test_dir/ --time-style=+': total 4" in f.read()
         f.close()
         f = open('/var/ossec/logs/archives/archives.json', 'r')
-        assert "ossec: output: 'ls -l /13-prevent_remote_commands/ls_test_dir/': -rw-r--r-- 1 root root 381 Jul 31 09:33 test.conf" in f.read()
+        assert "ossec: output: 'ls -l /13-prevent_remote_commands/ls_test_dir/ --time-style=+': -rw-r--r-- 1 root root 381 test.conf" in f.read()
         f.close()
         f = open('/var/ossec/logs/archives/archives.json', 'r')
         assert "ossec: output: 'cat /13-prevent_remote_commands/test.conf':\n<ossec_config>\n\n  <localfile>\n    <log_format>command</log_format>\n    <command>ls -l /13-prevent_remote_commands/ls_test_dir/</command>\n    <frequency>10</frequency>\n  </localfile>\n\n  <localfile>\n    <log_format>full_command</log_format>\n    <command>cat /13-prevent_remote_commands/test.conf</command>\n    <frequency>10</frequency>\n  </localfile>\n\n</ossec_config>" in f.read()
