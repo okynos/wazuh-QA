bb37df4684cd775c2f688ff2de0f5769891de628
okynos <pylott@gmail.com>
2018-07-31 11:02:10 +0200

    Preparing test to check with pytest

--- tests/logcollector/13-prevent_remote_commands/test/test.py
+++ tests/logcollector/13-prevent_remote_commands/test/test.py
@@ -1,7 +1,8 @@
 # test1AddLog.py
 def test_agent():
         f = open('/var/ossec/logs/ossec.log', 'r')
-        assert "ssh.log" in f.read()
+        assert "Monitoring output of command(10): ls -l /13-prevent_remote_commands" in f.read()
+        assert "Monitoring full output of command(10): cat /13-prevent_remote_commands/test.conf" in f.read()
 
 def test_manager():
         f = open('/var/ossec/logs/alerts/alerts.log', 'r')
