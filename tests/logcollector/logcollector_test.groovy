1e7f9c812f562e4dc3b3332dd347ba29e185763f
okynos <pylott@gmail.com>
2018-08-10 11:34:26 +0200

    Change environment and initial upload of logcollector complete test

---
+++ tests/logcollector/logcollector_test.groovy
@@ -0,0 +1,34 @@
+#!groovy
+
+def execution_node = params.EXECUTION_NODE
+def goal_version = params.WAZUH_VERSION
+def goal_branch = "master"
+def goal_api_branch = "master"
+def repo = "dev"
+
+// tests to pass
+def tests_to_pass = ['logcollector/01-add_log','logcollector/14-check_captured_command']
+
+node(execution_node){
+
+  stage("STAGE 1 - Launch functional test"){
+    tests_to_pass.eachWithIndex { test, index ->
+      stage("STAGE 1.$index - $test"){
+          build job: 'Logcollector_test_automatic', parameters: [
+            string(name: 'WAZUH_VERSION', value: "$goal_version"),
+            string(name: 'WHERE_DEPLOY', value: "env-docker"),
+            string(name: 'DEPLOY', value: "1mC-c_1aC-c"),
+            string(name: 'TEST', value: "logcollector/$test"),
+            string(name: 'EXECUTION_NODE', value: "$execution_node"),
+            string(name: 'KEY_NAME', value: "test.pem"),
+            string(name: 'MACHINE_USER', value: "ec2-user")
+          ]
+      }
+    }
+  }
+
+
+  stage("STAGE X - Finish"){
+
+  }
+}
