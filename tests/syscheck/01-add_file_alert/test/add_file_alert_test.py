4fbe710b02d9ef432d7321cd871498802f71be1b
okynos <pylott@gmail.com>
2018-08-13 12:44:57 +0200

    Added test results logs

--- tests/syscheck/01-add_file_alert/test/add_file_alert_test.py
+++ tests/syscheck/01-add_file_alert/test/add_file_alert_test.py
@@ -1,9 +1,9 @@
 def test_agent():
         f = open('/var/ossec/logs/ossec.log', 'r')
-        assert "PUT EVENT HERE" in f.read()
+        assert "ossec-syscheckd: INFO: Directory set for real time monitoring: '/add_file_alert'." in f.read()
         f.close()
 
 def test_manager():
         f = open('/var/ossec/logs/alerts/alerts.log', 'r')
-        assert "PUT ALERT HERE" in f.read()
+        assert "New file '/add_file_alert/add_file_alert.txt' added to the file system." in f.read()
         f.close()
